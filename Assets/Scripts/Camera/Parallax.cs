7292aba470f4a41bda5fa8a8529b26953bbf5b85
Alfredo Debasa Cabrera <alfredodebasa@gmail.com>
2020-05-03 15:02:53 -0400

    2nd Major Update
    Added parallax scrolling
    fixed level terrain
    made environment prettier

---
+++ Assets/Scripts/Camera/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPosition;
+    public GameObject mainCamera;
+    public float parallaxSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (mainCamera.transform.position.x * (1 - parallaxSpeed));
+        float distance = (mainCamera.transform.position.x * parallaxSpeed);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if(temp > startPosition + length)
+        {
+            startPosition += length;
+        }
+        else if(temp < startPosition - length){
+            startPosition -= length;
+        }
+    }
+}
