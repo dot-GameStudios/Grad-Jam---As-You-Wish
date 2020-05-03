996b3d40cbdc1ddb54fd6535802bec97c6f2e42e
Alfredo Debasa Cabrera <alfredodebasa@gmail.com>
2020-05-03 18:10:16 -0400

    Final Update
    finishing touches

---
+++ Assets/MoonScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MoonScript : MonoBehaviour
+{
+    public int KeyCount;
+    public GameObject EndGoal;
+    private Rigidbody2DTrigger RB2DTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB2DTrigger = GetComponent<Rigidbody2DTrigger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseKeyCount()
+    {
+        KeyCount++;
+        if(KeyCount == 4)
+        {
+            RevealPlatform();
+        }
+    }
+
+    public void RevealPlatform()
+    {
+        EndGoal.SetActive(true);
+    }
+
+    public void EndGame(string SceneName)
+    {
+        if (RB2DTrigger.Tag == "Player")
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
