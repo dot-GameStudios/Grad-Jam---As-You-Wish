3391421c5b8b878b7ceb15d5d5b3ae27fa1930bd
Joshua Sankarlal <joshua.sankarlal@uoit.net>
2020-05-03 15:09:32 -0400

    Player Animations!

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FlightAnimation()
+    {
+        animator.SetBool("Flying", true);
+    }
+
+    public void RunAnimation()
+    {
+        animator.SetBool("Flying", false);
+    }
+
+    public void FlipForward()
+    {
+        spriteRenderer.flipX = true;
+    }
+
+        public void FlipBackward()
+    {
+        spriteRenderer.flipX = false;
+    }
+}
